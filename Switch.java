2ab5a31f8e221c43d2d6426131949b951a78dcf7
Gauravpetwal <prasadpetwal02@gmail.com>
2023-07-10 15:31:53 +0530

    file

---
+++ Switch.java
@@ -0,0 +1,24 @@
+public class Switch
+{
+    public static void main(String[] args)
+    {
+        int s=3;
+        System.out.println("This is switch case");
+
+        switch (s)
+        {
+            case 1:
+                System.out.println("monday");
+                break;
+
+            case 2:
+                System.out.println("tuesday");
+                break;
+
+            default:
+                System.out.println("enter valid value:");
+        }
+
+    }
+
+}
